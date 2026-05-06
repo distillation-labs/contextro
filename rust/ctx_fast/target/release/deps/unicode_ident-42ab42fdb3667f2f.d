a086f7927fa19a96e81df1e3504f87ea9a7fdb80
Japneet Kalkat <japneet.kalkat@trailflow.ai>
2026-05-06 15:57:14 -0300

    Commit changes to: rust/ctx_fast/target/release/deps/unicode_ident-42ab42fdb3667f2f.d Enter commit message (or press Enter for default): rust/ctx_fast/target/release/deps/unindent-657ab616fe4b4e48.d

---
+++ rust/ctx_fast/target/release/deps/unicode_ident-42ab42fdb3667f2f.d
@@ -0,0 +1,8 @@
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/unicode_ident-42ab42fdb3667f2f.d: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/libunicode_ident-42ab42fdb3667f2f.rlib: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/libunicode_ident-42ab42fdb3667f2f.rmeta: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
+/Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
