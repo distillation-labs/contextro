01f0a922724cefa922e268b8a6e8eaeb395a821b
Japneet Kalkat <japneet.kalkat@trailflow.ai>
2026-05-06 15:57:13 -0300

    Commit changes to: rust/ctx_fast/target/release/deps/stable_deref_trait-b5a02d367319e29c.d Enter commit message (or press Enter for default): rust/ctx_fast/target/release/deps/syn-30665d8447f4f15f.d

---
+++ rust/ctx_fast/target/release/deps/stable_deref_trait-b5a02d367319e29c.d
@@ -0,0 +1,7 @@
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/stable_deref_trait-b5a02d367319e29c.d: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/libstable_deref_trait-b5a02d367319e29c.rlib: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/libstable_deref_trait-b5a02d367319e29c.rmeta: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs:
