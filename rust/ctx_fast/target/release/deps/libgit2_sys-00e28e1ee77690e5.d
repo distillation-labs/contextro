9cf1034a553345cb8de5930c233d776ae3a80e2d
Japneet Kalkat <japneet.kalkat@trailflow.ai>
2026-05-06 15:57:03 -0300

    Commit changes to: rust/ctx_fast/target/release/deps/libgit2_sys-00e28e1ee77690e5.d Enter commit message (or press Enter for default): rust/ctx_fast/target/release/deps/libglobset-d7cac530fbfbac63.rlib

---
+++ rust/ctx_fast/target/release/deps/libgit2_sys-00e28e1ee77690e5.d
@@ -0,0 +1,7 @@
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/libgit2_sys-00e28e1ee77690e5.d: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libgit2-sys-0.17.0+1.8.1/lib.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/liblibgit2_sys-00e28e1ee77690e5.rlib: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libgit2-sys-0.17.0+1.8.1/lib.rs
+
+/Users/japneetkalkat/Desktop/contextia-mcp/rust/contextia_fast/target/release/deps/liblibgit2_sys-00e28e1ee77690e5.rmeta: /Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libgit2-sys-0.17.0+1.8.1/lib.rs
+
+/Users/japneetkalkat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libgit2-sys-0.17.0+1.8.1/lib.rs:
